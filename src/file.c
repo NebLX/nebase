3a7b32e4696727da267a9d9e32da290587b312ef
Zhang Jingqiang <zh_jq@outlook.com>
2018-11-30 09:52:01 +0800

    Be compitable with glibc 2.28

--- src/file.c
+++ src/file.c
@@ -12,7 +12,9 @@
 
 #if defined(OS_LINUX)
 # include <linux/version.h>
-# if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+# if (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 28) || __GLIBC__ > 2
+#  define USE_STATX
+# elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
 #  include <sys/syscall.h>
 #  ifdef __NR_statx
 #   define USE_STATX
