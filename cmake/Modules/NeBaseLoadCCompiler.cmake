357574748801736648727fff93fffb162d4a658f
Zhang Jingqiang <zh_jq@outlook.com>
2018-12-22 14:15:46 +0800

    Update PGI required version

--- cmake/Modules/NeBaseLoadCCompiler.cmake
+++ cmake/Modules/NeBaseLoadCCompiler.cmake
@@ -84,11 +84,12 @@ elseif(CMAKE_C_COMPILER_ID STREQUAL "Intel")
     # analyze with codecov
   endif()
 elseif(CMAKE_C_COMPILER_ID STREQUAL "PGI")
-  if(CMAKE_C_COMPILER_VERSION VERSION_LESS "15.0")
-    message(SEND_ERROR "PGCC version >= 15.0 is required")
+  if(CMAKE_C_COMPILER_VERSION VERSION_LESS "18.10.2")
+    # _Atomic is still not supported, see <version dir>/include/_c_macros.h
+    message(SEND_ERROR "PGCC version > 18.10.1 is required")
   endif()
 
-  set(NeBase_C_FLAGS "${NeBase_C_FLAGS} -Minform=inform")
+  set(NeBase_C_FLAGS "${NeBase_C_FLAGS} -Minform=warn")
   # TODO
 elseif(CMAKE_C_COMPILER_ID STREQUAL "XL")
   if(CMAKE_C_COMPILER_VERSION VERSION_LESS "16.1")
