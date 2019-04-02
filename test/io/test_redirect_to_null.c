236d93029d7f1a5e8afc5bfd990787e2f0005aff
Zhang Jingqiang <zh_jq@outlook.com>
2019-04-02 22:43:03 +0800

    Do more strict check in test

--- test/io/test_redirect_to_null.c
+++ test/io/test_redirect_to_null.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+const char msg[] = "message to null\n";
+
 int main(void)
 {
 	if (neb_io_redirect_stdout(-1) != 0) {
@@ -10,7 +12,13 @@ int main(void)
 		return -1;
 	}
 
-	fprintf(stdout, "message to null\n");
+	int np = fprintf(stdout, "%s", msg);
+	if (np != (int)sizeof(msg) - 1) {
+		fprintf(stderr, "Failed to send msg to stdout\n");
+		return -1;
+	} else {
+		fprintf(stderr, "%d chars sent\n", np);
+	}
 	return 0;
 }
 
