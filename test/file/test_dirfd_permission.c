20087a59a8f94047a0a8086d51438125e5c656dc
Zhang Jingqiang <zh_jq@outlook.com>
2019-04-10 22:48:36 +0800

    fix testcase

--- test/file/test_dirfd_permission.c
+++ test/file/test_dirfd_permission.c
@@ -7,28 +7,28 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-const char dirname[] = "/tmp/.neb.testdir";
+const char t_dirname[] = "/tmp/.neb.testdir";
 mode_t dirmode = 0640;
 
 int main(void)
 {
 	int ret = 0;
 
-	if (mkdir(dirname, dirmode) == -1) {
+	if (mkdir(t_dirname, dirmode) == -1) {
 		perror("mkdir");
 		return -1;
 	}
 
-	int fd = neb_dir_open(dirname, NULL);
+	int fd = neb_dir_open(t_dirname, NULL);
 	if (fd == -1) {
-		fprintf(stderr, "Failed to open %s\n", dirname);
+		fprintf(stderr, "Failed to open %s\n", t_dirname);
 		ret = -1;
 		goto exit_rmdir;
 	}
 
 	neb_file_permission_t perm;
 	if (neb_dirfd_get_permission(fd, &perm) != 0) {
-		fprintf(stderr, "Failed to get dir permission for %s\n", dirname);
+		fprintf(stderr, "Failed to get dir permission for %s\n", t_dirname);
 		ret = -1;
 		goto exit_close_fd;
 	}
@@ -42,7 +42,7 @@ int main(void)
 exit_close_fd:
 	close(fd);
 exit_rmdir:
-	if (rmdir(dirname) == -1) {
+	if (rmdir(t_dirname) == -1) {
 		perror("rmdir");
 		ret = -1;
 	}
