81bc16bd9271dadf076837e24c1d6476fd3f9eac
Zhang Jingqiang <zh_jq@outlook.com>
2019-05-17 23:46:36 +0800

    Add os-release support to FreeBSD & NetBSD

--- cmake/Modules/GetFromOSRelease.sh
+++ cmake/Modules/GetFromOSRelease.sh
@@ -1,7 +1,65 @@
 #!/bin/sh
 
-[ -f /etc/os-release ] || exit 0
+set -e
 
-. /etc/os-release
+ENTRY=$1
+[ -n "${ENTRY}" ] || exit 0
 
-eval "echo \$$1"
+if [ -f /etc/os-release ]
+then
+	. /etc/os-release
+
+	eval "echo \$${ENTRY}"
+	exit 0
+fi
+
+get_for_freebsd()
+{
+	case ${ENTRY} in
+	'NAME')
+		sysctl kern.ostype | awk '{print $2}'
+		;;
+	'ID')
+		sysctl kern.ostype | awk '{print $2}' | tr 'A-Z' 'a-z'
+		;;
+	'VERSION')
+		uname -r
+		;;
+	'VERSION_ID')
+		uname -K
+		;;
+	*)
+		;;
+	esac
+}
+
+get_for_netbsd()
+{
+	case ${ENTRY} in
+	'NAME')
+		sysctl kern.ostype | awk '{print $2}'
+		;;
+	'ID')
+		sysctl kern.ostype | awk '{print $2}' | tr 'A-Z' 'a-z'
+		;;
+	'VERSION')
+		uname -r
+		;;
+	'VERSION_ID')
+		sysctl kern.osrevision
+		;;
+	*)
+		;;
+	esac
+}
+
+case $(uname -s) in
+'FreeBSD')
+	get_for_freebsd
+	;;
+'NetBSD')
+	get_for_netbsd
+	;;
+*)
+	;;
+esac
