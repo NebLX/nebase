13db57df55bc5bc8cf87fb935b42052ab83788db
Zhang Jingqiang <zh_jq@outlook.com>
2019-06-03 23:19:37 +0800

    evdp: add itimer testcase

---
+++ test/evdp/test_itimer_overrun.c
@@ -0,0 +1,69 @@
+
+#include <nebase/cdefs.h>
+#include <nebase/evdp.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+static neb_evdp_cb_ret_t on_wakeup(unsigned int ident _nattr_unused, long overrun, void *udata)
+{
+	int *count = udata;
+
+	switch (*count) {
+	case 0:
+		fprintf(stdout, "wakeup %d: overrun is %ld\n", *count, overrun);
+		*count += 1;
+		return NEB_EVDP_CB_CONTINUE;
+		break;
+	case 1:
+		fprintf(stdout, "wakeup %d: overrun is %ld\n", *count, overrun);
+		*count += 1;
+		usleep(20000);
+		return NEB_EVDP_CB_CONTINUE;
+		break;
+	case 2:
+		fprintf(stdout, "wakeup %d: overrun is %ld\n", *count, overrun);
+		return NEB_EVDP_CB_BREAK;
+		break;
+	default:
+		return NEB_EVDP_CB_BREAK;
+		break;
+	}
+}
+
+int main(void)
+{
+	int ret = 0;
+	neb_evdp_queue_t dq = neb_evdp_queue_create(0);
+	if (!dq) {
+		fprintf(stderr, "failed to create evdp queue\n");
+		return -1;
+	}
+
+	int count = 0;
+	neb_evdp_source_t ds = neb_evdp_source_new_itimer_ms(1, 10, on_wakeup);
+	if (!ds) {
+		fprintf(stderr, "failed to create itimer evdp source\n");
+		ret = -1;
+		goto exit_destroy_dq;
+	}
+	neb_evdp_source_set_udata(ds, &count);
+	if (neb_evdp_queue_attach(dq, ds) != 0) {
+		fprintf(stderr, "failed to attach itimer evdp source\n");
+		ret = -1;
+		goto exit_destroy_ds;
+	}
+
+	if (neb_evdp_queue_run(dq) != 0) {
+		fprintf(stderr, "error occured while running evdp queue\n");
+		ret = -1;
+	}
+
+	neb_evdp_queue_detach(dq, ds);
+exit_destroy_ds:
+	neb_evdp_source_del(ds);
+exit_destroy_dq:
+	neb_evdp_queue_destroy(dq);
+
+	return ret;
+}
