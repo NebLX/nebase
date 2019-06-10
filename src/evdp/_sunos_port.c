6c38363bec29b8a0366346df8ff0d44234056c3d
Zhang Jingqiang <zh_jq@outlook.com>
2019-06-10 20:11:09 +0800

    evdp: add SunOS port ro_fd source code

--- src/evdp/_sunos_port.c
+++ src/evdp/_sunos_port.c
@@ -22,9 +22,8 @@ struct evdp_source_timer_context {
 	struct itimerspec its;
 };
 
-struct evdp_source_fd_context {
-	int ctl_event;
-	int fd;
+struct evdp_source_ro_fd_context {
+	int associated;
 };
 
 void *evdp_create_queue_context(neb_evdp_queue_t q)
@@ -67,7 +66,7 @@ void evdp_destroy_queue_context(void *context)
 void evdp_queue_rm_pending_events(neb_evdp_queue_t q, neb_evdp_source_t s)
 {
 	void *s_got = NULL, *s_to_rm = s;
-	struct evdp_queue_context *c = q->context;
+	const struct evdp_queue_context *c = q->context;
 	if (!c->ee)
 		return;
 	for (int i = q->current_event; i < q->nevents; i++) {
@@ -80,7 +79,7 @@ void evdp_queue_rm_pending_events(neb_evdp_queue_t q, neb_evdp_source_t s)
 
 int evdp_queue_wait_events(neb_evdp_queue_t q, struct timespec *timeout)
 {
-	struct evdp_queue_context *c = q->context;
+	const struct evdp_queue_context *c = q->context;
 
 	uint_t nget = 1;
 	if (port_getn(c->fd, c->ee, q->batch_size, &nget, timeout) == -1) {
@@ -103,7 +102,7 @@ int evdp_queue_wait_events(neb_evdp_queue_t q, struct timespec *timeout)
 
 int evdp_queue_fetch_event(neb_evdp_queue_t q, struct neb_evdp_event *nee)
 {
-	struct evdp_queue_context *c = q->context;
+	const struct evdp_queue_context *c = q->context;
 
 	port_event_t *e = c->ee + q->current_event;
 	nee->event = e;
@@ -111,27 +110,30 @@ int evdp_queue_fetch_event(neb_evdp_queue_t q, struct neb_evdp_event *nee)
 	return 0;
 }
 
-static int do_associate_fd(const struct evdp_queue_context *qc, neb_evdp_source_t s)
+static int do_associate_ro_fd(const struct evdp_queue_context *qc, neb_evdp_source_t s)
 {
-	struct evdp_source_fd_context *sc = s->context;
-	if (port_associate(qc->fd, PORT_SOURCE_FD, sc->fd, sc->ctl_event, s) == -1) {
+	struct evdp_source_ro_fd_context *sc = s->context;
+	const struct evdp_conf_ro_fd *conf = s->conf;
+	if (port_associate(qc->fd, PORT_SOURCE_FD, conf->fd, POLLIN, s) == -1) {
 		neb_syslog(LOG_ERR, "port_associate: %m");
 		return -1;
 	}
+	sc->associated = 1;
 	return 0;
 }
 
 int evdp_queue_flush_pending_sources(neb_evdp_queue_t q)
 {
-	struct evdp_queue_context *qc = q->context;
+	const struct evdp_queue_context *qc = q->context;
 	int count = 0;
 	for (neb_evdp_source_t s = q->pending_qs->next; s; s = q->pending_qs->next) {
 		int ret = 0;
 		switch (s->type) {
 		case EVDP_SOURCE_RO_FD:
+			ret = do_associate_ro_fd(qc, s);
+			break;
 		case EVDP_SOURCE_OS_FD:
 		case EVDP_SOURCE_LT_FD:
-			ret = do_associate_fd(qc, s);
 			break;
 		// TODO add other source type here
 		default:
@@ -160,7 +162,7 @@ void *evdp_create_source_itimer_context(neb_evdp_source_t s)
 		return NULL;
 	}
 
-	struct evdp_conf_itimer *conf = s->conf;
+	const struct evdp_conf_itimer *conf = s->conf;
 	switch (s->type) {
 	case EVDP_SOURCE_ITIMER_SEC:
 		c->its.it_value.tv_sec = conf->sec;
@@ -193,7 +195,7 @@ void evdp_destroy_source_itimer_context(void *context)
 
 int evdp_source_itimer_attach(neb_evdp_queue_t q, neb_evdp_source_t s)
 {
-	struct evdp_queue_context *qc = q->context;
+	const struct evdp_queue_context *qc = q->context;
 	struct evdp_source_timer_context *sc = s->context;
 
 	port_notify_t pn = {
@@ -244,7 +246,7 @@ neb_evdp_cb_ret_t evdp_source_itimer_handle(struct neb_evdp_event *ne)
 	const port_event_t *e = ne->event;
 	int overrun = e->portev_events;
 
-	struct evdp_conf_itimer *conf = ne->source->conf;
+	const struct evdp_conf_itimer *conf = ne->source->conf;
 	if (conf->do_wakeup)
 		ret = conf->do_wakeup(conf->ident, overrun, ne->source->udata);
 
@@ -279,7 +281,7 @@ void evdp_destroy_source_abstimer_context(void *context)
 int evdp_source_abstimer_regulate(neb_evdp_source_t s)
 {
 	struct evdp_source_timer_context *c = s->context;
-	struct evdp_conf_abstimer *conf = s->conf;
+	const struct evdp_conf_abstimer *conf = s->conf;
 
 	time_t abs_ts;
 	int delta_sec;
@@ -303,7 +305,7 @@ int evdp_source_abstimer_regulate(neb_evdp_source_t s)
 
 int evdp_source_abstimer_attach(neb_evdp_queue_t q, neb_evdp_source_t s)
 {
-	struct evdp_queue_context *qc = q->context;
+	const struct evdp_queue_context *qc = q->context;
 	struct evdp_source_timer_context *sc = s->context;
 
 	port_notify_t pn = {
@@ -354,9 +356,80 @@ neb_evdp_cb_ret_t evdp_source_abstimer_handle(struct neb_evdp_event *ne)
 	const port_event_t *e = ne->event;
 	int overrun = e->portev_events;
 
-	struct evdp_conf_itimer *conf = ne->source->conf;
+	const struct evdp_conf_itimer *conf = ne->source->conf;
 	if (conf->do_wakeup)
 		ret = conf->do_wakeup(conf->ident, overrun, ne->source->udata);
+
+	return ret;
+}
+
+void *evdp_create_source_ro_fd_context(neb_evdp_source_t s)
+{
+	struct evdp_source_ro_fd_context *c = calloc(1, sizeof(struct evdp_source_ro_fd_context));
+	if (!c) {
+		neb_syslog(LOG_ERR, "calloc: %m");
+		return NULL;
+	}
+
+	c->associated = 0;
+	s->pending = 0;
+
+	return c;
+}
+
+void evdp_destroy_source_ro_fd_context(void *context)
+{
+	struct evdp_source_ro_fd_context *c = context;
+
+	free(c);
+}
+
+int evdp_source_ro_fd_attach(neb_evdp_queue_t q, neb_evdp_source_t s)
+{
+	EVDP_SLIST_PENDING_INSERT(q, s);
+
+	return 0;
+}
+
+void evdp_source_ro_fd_detach(neb_evdp_queue_t q, neb_evdp_source_t s)
+{
+	const struct evdp_queue_context *qc = q->context;
+	const struct evdp_conf_ro_fd *conf = s->conf;
+	struct evdp_source_ro_fd_context *sc = s->context;
+
+	if (sc->associated) {
+		if (port_dissociate(qc->fd, PORT_SOURCE_FD, conf->fd) == -1)
+			neb_syslog(LOG_ERR, "port_dissociate: %m");
+		sc->associated = 0;
+	}
+}
+
+neb_evdp_cb_ret_t evdp_source_ro_fd_handle(struct neb_evdp_event *ne)
+{
+	neb_evdp_cb_ret_t ret = NEB_EVDP_CB_CONTINUE;
+
+	struct evdp_source_ro_fd_context *sc = ne->source->context;
+	sc->associated = 0;
+
+	const port_event_t *e = ne->event;
+
+	struct evdp_conf_ro_fd *conf = ne->source->conf;
+	if (e->portev_events & POLLIN) {
+		ret = conf->do_read(e->portev_object, ne->source->udata);
+		if (ret != NEB_EVDP_CB_CONTINUE)
+			return ret;
+	}
+	if (e->portev_events & POLLHUP) {
+		ret = conf->do_hup(e->portev_object, ne->source->udata);
+		if (ret != NEB_EVDP_CB_BREAK)
+			ret = NEB_EVDP_CB_REMOVE;
+	}
+	if (ret == NEB_EVDP_CB_CONTINUE) {
+		neb_evdp_queue_t q = ne->source->q_in_use;
+		EVDP_SLIST_REMOVE(ne->source);
+		q->stats.running--;
+		EVDP_SLIST_PENDING_INSERT(q, ne->source);
+	}
 
 	return ret;
 }
