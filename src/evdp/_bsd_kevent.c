77a7203fdbe924e20d4dbeb5c62b38757d8d771f
Zhang Jingqiang <zh_jq@outlook.com>
2019-06-26 08:49:53 +0800

    evdp: add full kevent os_fd code

--- src/evdp/_bsd_kevent.c
+++ src/evdp/_bsd_kevent.c
@@ -586,21 +586,37 @@ int evdp_source_os_fd_attach(neb_evdp_queue_t q, neb_evdp_source_t s)
 	return 0;
 }
 
+static int do_del_os_fd_rd(const struct evdp_queue_context *qc, struct evdp_source_os_fd_context *sc)
+{
+	sc->rd.ctl_event.flags = EV_DISABLE | EV_DELETE;
+	if (kevent(qc->fd, &sc->rd.ctl_event, 1, NULL, 0, NULL) == -1 && errno != ENOENT) {
+		neb_syslog(LOG_ERR, "kevent: %m");
+		return -1;
+	}
+	sc->rd.added = 0;
+	return 0;
+}
+
+static int do_del_os_fd_wr(const struct evdp_queue_context *qc, struct evdp_source_os_fd_context *sc)
+{
+	sc->wr.ctl_event.flags = EV_DISABLE | EV_DELETE;
+	if (kevent(qc->fd, &sc->wr.ctl_event, 1, NULL, 0, NULL) == -1 && errno != ENOENT) {
+		neb_syslog(LOG_ERR, "kevent: %m");
+		return -1;
+	}
+	sc->wr.added = 0;
+	return 0;
+}
+
 void evdp_source_os_fd_detach(neb_evdp_queue_t q, neb_evdp_source_t s)
 {
 	const struct evdp_queue_context *qc = q->context;
 	struct evdp_source_os_fd_context *sc = s->context;
 
-	if (sc->rd.added) {
-		sc->rd.ctl_event.flags = EV_DISABLE | EV_DELETE;
-		if (kevent(qc->fd, &sc->rd.ctl_event, 1, NULL, 0, NULL) == -1 && errno != ENOENT)
-			neb_syslog(LOG_ERR, "kevent: %m");
-	}
-	if (sc->wr.added) {
-		sc->wr.ctl_event.flags = EV_DISABLE | EV_DELETE;
-		if (kevent(qc->fd, &sc->wr.ctl_event, 1, NULL, 0, NULL) == -1 && errno != ENOENT)
-			neb_syslog(LOG_ERR, "kevent: %m");
-	}
+	if (sc->rd.added)
+		do_del_os_fd_rd(qc, sc);
+	if (sc->wr.added)
+		do_del_os_fd_wr(qc, sc);
 }
 
 neb_evdp_cb_ret_t evdp_source_os_fd_handle(const struct neb_evdp_event *ne)
@@ -662,4 +678,96 @@ neb_evdp_cb_ret_t evdp_source_os_fd_handle(const struct neb_evdp_event *ne)
 	}
 
 	return ret;
+}
+
+void evdp_source_os_fd_init_read(neb_evdp_source_t s, neb_evdp_io_handler_t rf)
+{
+	struct evdp_source_os_fd_context *sc = s->context;
+	if (rf)
+		sc->rd.to_add = 1;
+	else
+		sc->rd.to_add = 0;
+}
+
+void evdp_source_os_fd_init_write(neb_evdp_source_t s, neb_evdp_io_handler_t rf)
+{
+	struct evdp_source_os_fd_context *sc = s->context;
+	if (rf)
+		sc->wr.to_add = 1;
+	else
+		sc->wr.to_add = 0;
+}
+
+int evdp_source_os_fd_reset_read(neb_evdp_source_t s)
+{
+	struct evdp_source_os_fd_context *sc = s->context;
+	if (sc->rd.added) {
+		return 0;
+	} else {
+		sc->rd.to_add = 1;
+		sc->rd.ctl_event.flags = EV_ADD | EV_ENABLE | EV_ONESHOT;
+		if (!s->pending) { // Make sure add to pending
+			neb_evdp_queue_t q = s->q_in_use;
+			EVDP_SLIST_REMOVE(s);
+			q->stats.running--;
+			EVDP_SLIST_PENDING_INSERT(q, s);
+		}
+	}
+	return 0;
+}
+
+int evdp_source_os_fd_reset_write(neb_evdp_source_t s)
+{
+	struct evdp_source_os_fd_context *sc = s->context;
+	if (sc->wr.added) {
+		return 0;
+	} else {
+		sc->wr.to_add = 1;
+		sc->wr.ctl_event.flags = EV_ADD | EV_ENABLE | EV_ONESHOT;
+		if (!s->pending) { // Make sure add to pending
+			neb_evdp_queue_t q = s->q_in_use;
+			EVDP_SLIST_REMOVE(s);
+			q->stats.running--;
+			EVDP_SLIST_PENDING_INSERT(q, s);
+		}
+	}
+	return 0;
+}
+
+int evdp_source_os_fd_unset_read(neb_evdp_source_t s)
+{
+	struct evdp_source_os_fd_context *sc = s->context;
+	if (sc->rd.added) {
+		return do_del_os_fd_rd(s->q_in_use->context, sc);
+	} else if (s->pending) {
+		sc->rd.to_add = 0;
+		if (sc->wr.to_add)
+			return 0;
+		neb_evdp_queue_t q = s->q_in_use;
+		EVDP_SLIST_REMOVE(s);
+		q->stats.pending--;
+		EVDP_SLIST_RUNNING_INSERT(q, s);
+	} else {
+		sc->rd.to_add = 0;
+	}
+	return 0;
+}
+
+int evdp_source_os_fd_unset_write(neb_evdp_source_t s)
+{
+	struct evdp_source_os_fd_context *sc = s->context;
+	if (sc->wr.added) {
+		return do_del_os_fd_wr(s->q_in_use->context, sc);
+	} else if (s->pending) {
+		sc->wr.to_add = 0;
+		if (sc->rd.to_add)
+			return 0;
+		neb_evdp_queue_t q = s->q_in_use;
+		EVDP_SLIST_REMOVE(s);
+		q->stats.pending--;
+		EVDP_SLIST_RUNNING_INSERT(q, s);
+	} else {
+		sc->wr.to_add = 0;
+	}
+	return 0;
 }
