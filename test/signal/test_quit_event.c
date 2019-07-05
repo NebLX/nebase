3403a3eb50caac472cb6db755d33b9347b2f4e7e
Zhang Jingqiang <zh_jq@outlook.com>
2019-07-05 21:38:37 +0800

    Simplify signal testcase

--- test/signal/test_quit_event.c
+++ test/signal/test_quit_event.c
@@ -6,7 +6,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
-#include <sys/wait.h>
 
 int main(void)
 {
@@ -20,36 +19,16 @@ int main(void)
 	}
 
 	int ret = 0;
-	int cpid = fork();
-	if (cpid == -1) {
-		perror("fork");
+	if (raise(SIGTERM) != 0) {
+		perror("raise");
+		return -1;
+	}
+
+	if (!(thread_events & T_E_QUIT)) {
+		fprintf(stderr, "No quit event set after timeout\n");
 		ret = -1;
-	} else if (cpid == 0) {
-		ret = kill(getppid(), SIGTERM);
-		if (ret == -1)
-			perror("kill");
 	} else {
-		for (int i = 0; i < 1000; i++) {
-			if (thread_events & T_E_QUIT)
-				break;
-			usleep(1000);
-		}
-
-		if (!(thread_events & T_E_QUIT)) {
-			fprintf(stderr, "No quit event set after timeout\n");
-			ret = -1;
-		} else {
-			fprintf(stdout, "quit event set\n");
-		}
-
-		int wstatus;
-		if (waitpid(cpid, &wstatus, 0) == -1) {
-			perror("waitpid");
-			ret = -1;
-		} else if (!WIFEXITED(wstatus) || WEXITSTATUS(wstatus) != 0) {
-			fprintf(stderr, "child exit with error\n");
-			ret = -1;
-		}
+		fprintf(stdout, "quit event set\n");
 	}
 
 	return ret;
