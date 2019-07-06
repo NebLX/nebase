6cca772067c14de614d736d9b496ec59763169ce
Zhang Jingqiang <zh_jq@outlook.com>
2019-07-07 00:05:44 +0800

    fix printf check

--- cmake/CompileTests/printf_strerr.cmake
+++ cmake/CompileTests/printf_strerr.cmake
@@ -1,6 +1,7 @@
 
 try_run(PRINTF_STRERR_RUN_RET PRINTF_STRERR_COMPILE_RET
   ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/CompileTests/printf_strerr.c
+  RUN_OUTPUT_VARIABLE PRINTF_STRERR_RUN_OUTPUT
 )
 
 if(NOT PRINTF_STRERR_COMPILE_RET)
@@ -9,7 +10,9 @@ else()
   if(PRINTF_STRERR_RUN_RET STREQUAL "FAILED_TO_RUN")
     message(FATAL_ERROR "Failed to run the printf_strerr test program")
   elseif(PRINTF_STRERR_RUN_RET EQUAL 0)
-    set(PRINTF_SUPPORT_STRERR ON CACHE INTERNAL "printf %m support")
+    if(NOT PRINTF_STRERR_RUN_OUTPUT STREQUAL "m")
+      set(PRINTF_SUPPORT_STRERR ON CACHE INTERNAL "printf %m support")
+    endif()
   endif()
 
   if(PRINTF_SUPPORT_STRERR)
