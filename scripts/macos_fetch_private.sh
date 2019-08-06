7db5ca3f068f2182a3db1832f15ce48b5c498cbc
Zhang Jingqiang <zh_jq@outlook.com>
2019-08-06 22:38:40 +0800

    Add fetch private script for MacOS

---
+++ scripts/macos_fetch_private.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+SRC_DIR="$(dirname $0)/.."
+
+temp_file=$(mktemp /tmp/nebase.tmp.XXXXXX)
+
+clean_temp_file()
+{
+	[ -z "${temp_file}" ] || rm "${temp_file}"
+}
+
+trap clean_temp_file EXIT
+
+echo "Checking XNU Version ..."
+
+xnu_version=$(curl "https://opensource.apple.com/source/xnu/" | sed -n 's/.*>\(xnu-[1-9][0-9.]*\)\/.*/\1/p' | sort -V | tail -n 1)
+
+echo "XNU Version: ${xnu_version}"
+
+echo "Fetching sys/unpcb.h ..."
+
+curl "https://opensource.apple.com/source/xnu/${xnu_version}/bsd/sys/unpcb.h" -o "${temp_file}"
+
+echo "Installing sys/unpcb.h ..."
+install -d -m 755 "${SRC_DIR}/private/include/sys"
+install -Cv -m 644 "${temp_file}" "${SRC_DIR}/private/include/sys/unpcb.h"
+
