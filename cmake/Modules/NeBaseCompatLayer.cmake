d1542611427be456d6a4debd11c70b6a7d63ca4b
Zhang Jingqiang <zh_jq@outlook.com>
2019-09-27 22:16:13 +0800

    Add embed gnulib ostack if not available

--- cmake/Modules/NeBaseCompatLayer.cmake
+++ cmake/Modules/NeBaseCompatLayer.cmake
@@ -6,3 +6,8 @@ include(FeatureSummary)
 
 CHECK_INCLUDE_FILE("sys/rbtree.h" HAVE_SYS_RBTREE)
 add_feature_info(HAVE_SYS_RBTREE HAVE_SYS_RBTREE "sys/rbtree.h is available")
+
+# Gnulib obstack
+
+CHECK_INCLUDE_FILE("obstack.h" HAVE_SYS_OBSTACK)
+add_feature_info(HAVE_SYS_OBSTACK HAVE_SYS_OBSTACK "obstack.h is available")
