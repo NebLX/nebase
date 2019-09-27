deb2acdd6f9caa2a2b904d8caa237c4c92c9eda7
Zhang Jingqiang <zh_jq@outlook.com>
2019-09-27 23:20:07 +0800

    Add gnulib hash

---
+++ compat/hash/config.h
@@ -0,0 +1,10 @@
+
+#define USE_OBSTACK 1
+
+#define _GL_INLINE_HEADER_BEGIN
+#define _GL_INLINE_HEADER_END
+#define _GL_INLINE __inline__
+
+#ifndef _GL_ATTRIBUTE_CONST
+# define _GL_ATTRIBUTE_CONST __attribute__((__const__))
+#endif
