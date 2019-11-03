74c9cc39321600bf5666d6c1e5fc38c0b00d5193
Zhang Jingqiang <zh_jq@outlook.com>
2019-11-03 23:45:59 +0800

    Add pty util compatible functions

---
+++ include/nebase/pty.h
@@ -0,0 +1,24 @@
+
+#ifndef NEB_PTY_H
+#define NEB_PTY_H 1
+
+#include "cdefs.h"
+
+#include <termios.h>
+
+struct neb_pty_winsize {
+	unsigned short int ws_row;
+	unsigned short int ws_col;
+	unsigned short int ws_xpixel;
+	unsigned short int ws_ypixel;
+};
+
+extern int neb_pty_openpty(int *amaster, int *aslave, char *name,
+                           const struct termios *termp,
+                           const struct neb_pty_winsize *winp)
+	_nattr_warn_unused_result _nattr_nonnull((1, 2));
+
+extern int neb_pty_login_tty(int slave_fd)
+	_nattr_warn_unused_result;
+
+#endif
---
+++ src/pty.c
@@ -0,0 +1,60 @@
+
+#include "options.h"
+
+#include <nebase/syslog.h>
+#include <nebase/pty.h>
+
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#if defined(OS_LINUX)
+# include <pty.h>
+# include <utmp.h>
+#elif defined(OS_FREEBSD) || defined(OS_DFLYBSD)
+# include <libutil.h>
+#elif defined(OS_NETBSD) || defined(OS_OPENBSD)
+# include <util.h>
+#elif defined(OS_SOLARIS)
+// nothing
+#else
+# error "fix me"
+#endif
+
+int neb_pty_openpty(int *amaster, int *aslave, char *name,
+                    const struct termios *termp,
+                    const struct neb_pty_winsize *winp)
+{
+	struct winsize win, *real_winp = NULL;
+	if (winp) {
+		win.ws_col = winp->ws_col;
+		win.ws_row = winp->ws_row;
+		win.ws_xpixel = winp->ws_xpixel;
+		win.ws_ypixel = winp->ws_ypixel;
+		real_winp = &win;
+	}
+
+#if defined(OS_LINUX) || defined(OSTYPE_BSD) || defined(OS_SOLARIS)
+	if (openpty(amaster, aslave, name, termp, real_winp) == -1) {
+		neb_syslog(LOG_ERR, "openpty: %m");
+		return -1;
+	}
+#else
+# error "fix me"
+#endif
+
+	return 0;
+}
+
+int neb_pty_login_tty(int slave_fd)
+{
+#if defined(OS_LINUX) || defined(OSTYPE_BSD) || defined(OS_SOLARIS)
+	if (login_tty(slave_fd) == -1) {
+		neb_syslog(LOG_ERR, "login_tty: %m");
+		return -1;
+	}
+#else
+# error "fix me"
+#endif
+	return 0;
+}
