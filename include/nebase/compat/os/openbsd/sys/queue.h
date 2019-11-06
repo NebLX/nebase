7f6165f7b8578c28340faa2d57e275e53cb1e5f4
Zhang Jingqiang <zh_jq@outlook.com>
2019-11-06 23:57:11 +0800

    fix end macro

--- include/nebase/compat/os/openbsd/sys/queue.h
+++ include/nebase/compat/os/openbsd/sys/queue.h
@@ -101,4 +101,6 @@ struct {                                                                \
                 ((struct type *)(void *)                                \
                 ((char *)((head)->stqh_last) - offsetof(struct type, field))))
 
+#endif /* STAILQ_HEAD */
+
 #endif
