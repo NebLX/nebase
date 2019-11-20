3508ecea6edb5abe214fcda55e537a17d501a1cf
Zhang Jingqiang <zh_jq@outlook.com>
2019-11-20 20:59:28 +0800

    make ping testcase work

--- test/sock/test_raw4_ping_localhost.c
+++ test/sock/test_raw4_ping_localhost.c
@@ -1,12 +1,15 @@
 
 #include <nebase/sock/raw.h>
+#include <nebase/sock/inet.h>
 #include <nebase/sock/csum.h>
 #include <nebase/evdp.h>
 #include <nebase/events.h>
 #include <nebase/random.h>
+#include <nebase/time.h>
 
 #include <stdio.h>
 #include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
 
 static neb_evdp_queue_t evdp_queue = NULL;
 static neb_evdp_timer_t evdp_timer = NULL;
@@ -16,6 +19,16 @@ static neb_evdp_timer_point quit_tp = NULL;
 
 static int has_error = 0;
 static int raw_fd = -1;
+
+static uint16_t sent_icmp_id = 0;
+static uint16_t sent_icmp_seq = 0;
+
+struct ipv4_data {
+	struct sockaddr_in peer_addr;
+	struct sockaddr_in local_addr;
+	struct timespec ts;
+	unsigned int ifindex;
+};
 
 static neb_evdp_timeout_ret_t send_pkt(void *udata _nattr_unused)
 {
@@ -27,6 +40,8 @@ static neb_evdp_timeout_ret_t send_pkt(void *udata _nattr_unused)
 	ih->icmp_code = 0;
 	ih->icmp_id = neb_random_uniform(UINT16_MAX);
 	ih->icmp_seq = neb_random_uniform(UINT16_MAX);
+	sent_icmp_id = ih->icmp_id;
+	sent_icmp_seq = ih->icmp_seq;
 
 	neb_sock_csum_icmp4_fill(ih, sizeof(buf));
 
@@ -40,6 +55,108 @@ static neb_evdp_timeout_ret_t send_pkt(void *udata _nattr_unused)
 	}
 
 	return NEB_EVDP_TIMEOUT_KEEP;
+}
+
+static int parse_cmsg(int level, int type, const u_char *data, size_t len _nattr_unused, void *udata)
+{
+	struct ipv4_data *d = udata;
+
+	if (level != NEB_CMSG_LEVEL_COMPAT)
+		return 0;
+
+	switch (type) {
+	case NEB_CMSG_TYPE_LOOP_END:
+		if (!d->ts.tv_sec)
+			return neb_time_gettimeofday(&d->ts);
+		break;
+	case NEB_CMSG_TYPE_TIMESTAMP:
+	{
+		const struct timespec *t = (const struct timespec *)data;
+		d->ts.tv_sec = t->tv_sec;
+		d->ts.tv_nsec = t->tv_nsec;
+	}
+		break;
+	case NEB_CMSG_TYPE_IP4IFINDEX:
+		d->ifindex = *(const int *)data;
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+static neb_evdp_cb_ret_t recv_pkt(int fd, void *udata _nattr_unused, const void *context _nattr_unused)
+{
+	static char buf[UINT16_MAX];
+
+	struct ipv4_data d = NEB_STRUCT_INITIALIZER;
+	d.peer_addr.sin_family = AF_INET;
+
+	struct iovec iov = {.iov_base = buf, .iov_len = sizeof(buf)};
+
+	struct neb_sock_msghdr m = {
+		.msg_peer = (struct sockaddr *)&d.peer_addr,
+		.msg_iov = &iov,
+		.msg_iovlen = 1,
+		.msg_control_cb = parse_cmsg,
+		.msg_udata = &d,
+	};
+
+	ssize_t nr = neb_sock_inet_recvmsg(fd, &m);
+	if (nr == -1) {
+		fprintf(stderr, "failed to recv icmp packet\n");
+		return NEB_EVDP_CB_BREAK_ERR;
+	}
+	size_t left = nr;
+
+	if (left < (int)sizeof(struct ip)) {
+		fprintf(stderr, "Invalid IPv4 msg: no valid header\n");
+		return NEB_EVDP_CB_CONTINUE;
+	}
+	struct ip *iphdr = (struct ip *)buf;
+	size_t pktlen = neb_sock_raw4_get_pktlen(iphdr);
+	if (pktlen > left) {
+		fprintf(stderr, "Invalid IPv4 msg: pkt len %zu is larger than read size %zu\n", pktlen, left);
+		return NEB_EVDP_CB_CONTINUE;
+	}
+	size_t iphdr_len = iphdr->ip_hl << 2;
+	if (iphdr_len > left) {
+		fprintf(stderr, "Invalid IPv4 msg: hdr len %zu is larger than read size %zu\n", iphdr_len, left);
+		return NEB_EVDP_CB_CONTINUE;
+	}
+	d.local_addr.sin_family = AF_INET;
+	d.local_addr.sin_addr.s_addr = iphdr->ip_dst.s_addr;
+
+	left -= iphdr_len;
+	if (left < (int)sizeof(struct icmp)) {
+		fprintf(stderr, "Invalid ICMP msg: no valid header\n");
+		return NEB_EVDP_CB_CONTINUE;
+	}
+	struct icmp *icmphdr = (struct icmp *)(buf + iphdr_len);
+	if (icmphdr->icmp_type != ICMP_ECHOREPLY)
+		return NEB_EVDP_CB_CONTINUE;
+
+	fprintf(stdout, "recieved one icmp echo reply packet\n");
+	if (iphdr->ip_dst.s_addr != htonl(INADDR_LOOPBACK) ||
+	    icmphdr->icmp_id != sent_icmp_id ||
+	    icmphdr->icmp_seq != sent_icmp_seq)
+		return NEB_EVDP_CB_CONTINUE;
+
+	char peer_addr_s[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &d.peer_addr.sin_addr, peer_addr_s, sizeof(peer_addr_s));
+	char local_addr_s[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &d.local_addr.sin_addr, local_addr_s, sizeof(local_addr_s));
+	fprintf(stdout, "%lds %ldns %s <- %s, ifindex %u, size %zu\n",
+		d.ts.tv_sec, d.ts.tv_nsec, local_addr_s, peer_addr_s, d.ifindex, left);
+	return NEB_EVDP_CB_BREAK_EXP;
+}
+
+static neb_evdp_cb_ret_t fd_hup(int fd, void *udata, const void *context)
+{
+	neb_evdp_sock_log_on_hup(fd, udata, context);
+	has_error = 1;
+	return NEB_EVDP_CB_BREAK_ERR;
 }
 
 static neb_evdp_timeout_ret_t quit_timeout(void *udata _nattr_unused)
@@ -78,7 +195,17 @@ int main(void)
 		goto exit_fail;
 	}
 
-	// TODO
+	neb_evdp_source_t s = neb_evdp_source_new_ro_fd(raw_fd, recv_pkt, fd_hup);
+	if (!s) {
+		fprintf(stderr, "failed to create source for the raw socket\b");
+		goto exit_fail;
+	}
+	neb_evdp_source_set_on_remove(s, neb_evdp_source_del);
+	if (neb_evdp_queue_attach(evdp_queue, s) != 0) {
+		fprintf(stderr, "failed to attach source\n");
+		neb_evdp_source_del(s);
+		goto exit_fail;
+	}
 
 	if (neb_evdp_queue_run(evdp_queue) != 0) {
 		fprintf(stderr, "error occure while running evdp queue\n");
@@ -88,13 +215,8 @@ int main(void)
 		ret = -1;
 
 exit_clean:
-	if (evdp_timer) {
-		if (send_tp)
-			neb_evdp_timer_del_point(evdp_timer, send_tp);
-		if (quit_tp)
-			neb_evdp_timer_del_point(evdp_timer, quit_tp);
+	if (evdp_timer)
 		neb_evdp_timer_destroy(evdp_timer);
-	}
 	if (evdp_queue)
 		neb_evdp_queue_destroy(evdp_queue);
 	return ret;
--- test/sock/test_raw6_ping_localhost.c
+++ test/sock/test_raw6_ping_localhost.c
@@ -1,12 +1,15 @@
 
 
 #include <nebase/sock/raw.h>
+#include <nebase/sock/inet.h>
 #include <nebase/evdp.h>
 #include <nebase/events.h>
 #include <nebase/random.h>
+#include <nebase/time.h>
 
 #include <stdio.h>
 #include <netinet/icmp6.h>
+#include <arpa/inet.h>
 
 static neb_evdp_queue_t evdp_queue = NULL;
 static neb_evdp_timer_t evdp_timer = NULL;
@@ -16,6 +19,16 @@ static neb_evdp_timer_point quit_tp = NULL;
 
 static int has_error = 0;
 static int raw_fd = -1;
+
+static uint16_t sent_icmp_id = 0;
+static uint16_t sent_icmp_seq = 0;
+
+struct ipv6_data {
+	struct sockaddr_in6 peer_addr;
+	struct sockaddr_in6 local_addr;
+	struct timespec ts;
+	unsigned int ifindex;
+};
 
 static neb_evdp_timeout_ret_t send_pkt(void *udata _nattr_unused)
 {
@@ -27,6 +40,8 @@ static neb_evdp_timeout_ret_t send_pkt(void *udata _nattr_unused)
 	ih->icmp6_code = 0;
 	ih->icmp6_id = neb_random_uniform(UINT16_MAX);
 	ih->icmp6_seq = neb_random_uniform(UINT16_MAX);
+	sent_icmp_id = ih->icmp6_id;
+	sent_icmp_seq = ih->icmp6_seq;
 	// no need to calc checksum
 
 	ssize_t nw = neb_sock_raw_icmp6_send(raw_fd, buf, sizeof(buf), &in6addr_loopback, &in6addr_loopback, 0);
@@ -37,6 +52,103 @@ static neb_evdp_timeout_ret_t send_pkt(void *udata _nattr_unused)
 	}
 
 	return NEB_EVDP_TIMEOUT_KEEP;
+}
+
+static int parse_cmsg(int level, int type, const u_char *data, size_t len _nattr_unused, void *udata)
+{
+	struct ipv6_data *d = udata;
+
+	switch (level) {
+	case NEB_CMSG_LEVEL_COMPAT:
+		switch (type) {
+		case NEB_CMSG_TYPE_LOOP_END:
+			if (!d->ts.tv_sec)
+				return neb_time_gettimeofday(&d->ts);
+			break;
+		case NEB_CMSG_TYPE_TIMESTAMP:
+		{
+			const struct timespec *t = (const struct timespec *)data;
+			d->ts.tv_sec = t->tv_sec;
+			d->ts.tv_nsec = t->tv_nsec;
+		}
+			break;
+		default:
+			break;
+		}
+		break;
+	case IPPROTO_IPV6:
+		switch (type) {
+		case IPV6_PKTINFO:
+		{
+			const struct in6_pktinfo *info = (const struct in6_pktinfo *)data;
+			d->local_addr.sin6_family = AF_INET6;
+			memcpy(&d->local_addr.sin6_addr, &info->ipi6_addr, sizeof(struct in6_addr));
+			d->ifindex = info->ipi6_ifindex;
+		}
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+static neb_evdp_cb_ret_t recv_pkt(int fd, void *udata _nattr_unused, const void *context _nattr_unused)
+{
+	static char buf[UINT16_MAX];
+
+	struct ipv6_data d = NEB_STRUCT_INITIALIZER;
+	d.peer_addr.sin6_family = AF_INET6;
+
+	struct iovec iov = {.iov_base = buf, .iov_len = sizeof(buf)};
+
+	struct neb_sock_msghdr m = {
+		.msg_peer = (struct sockaddr *)&d.peer_addr,
+		.msg_iov = &iov,
+		.msg_iovlen = 1,
+		.msg_control_cb = parse_cmsg,
+		.msg_udata = &d,
+	};
+
+	ssize_t nr = neb_sock_inet_recvmsg(fd, &m);
+	if (nr == -1) {
+		fprintf(stderr, "failed to recv icmp packet\n");
+		return NEB_EVDP_CB_BREAK_ERR;
+	}
+	size_t left = nr;
+
+	if (left < (int)sizeof(struct icmp6_hdr)) {
+		fprintf(stderr, "Invalid ICMPv6 msg: no valid header\n");
+		return NEB_EVDP_CB_CONTINUE;
+	}
+	struct icmp6_hdr *ih = (struct icmp6_hdr *)buf;
+	if (ih->icmp6_type != ICMP6_ECHO_REPLY)
+		return NEB_EVDP_CB_CONTINUE;
+
+	fprintf(stdout, "recieved one icmp echo reply packet\n");
+	if (memcmp(&d.peer_addr.sin6_addr, &in6addr_loopback, sizeof(struct in6_addr)) != 0 ||
+	    ih->icmp6_id != sent_icmp_id ||
+	    ih->icmp6_seq != sent_icmp_seq)
+		return NEB_EVDP_CB_CONTINUE;
+
+	char peer_addr_s[INET6_ADDRSTRLEN];
+	inet_ntop(AF_INET6, &d.peer_addr.sin6_addr, peer_addr_s, sizeof(peer_addr_s));
+	char local_addr_s[INET6_ADDRSTRLEN];
+	inet_ntop(AF_INET6, &d.local_addr.sin6_addr, local_addr_s, sizeof(local_addr_s));
+	fprintf(stdout, "%lds %ldns %s <- %s, ifindex %u, size %zu\n",
+		d.ts.tv_sec, d.ts.tv_nsec, local_addr_s, peer_addr_s, d.ifindex, left);
+	return NEB_EVDP_CB_BREAK_EXP;
+}
+
+static neb_evdp_cb_ret_t fd_hup(int fd, void *udata, const void *context)
+{
+	neb_evdp_sock_log_on_hup(fd, udata, context);
+	has_error = 1;
+	return NEB_EVDP_CB_BREAK_ERR;
 }
 
 static neb_evdp_timeout_ret_t quit_timeout(void *udata _nattr_unused)
@@ -75,7 +187,17 @@ int main(void)
 		goto exit_fail;
 	}
 
-	// TODO
+	neb_evdp_source_t s = neb_evdp_source_new_ro_fd(raw_fd, recv_pkt, fd_hup);
+	if (!s) {
+		fprintf(stderr, "failed to create source for the raw socket\b");
+		goto exit_fail;
+	}
+	neb_evdp_source_set_on_remove(s, neb_evdp_source_del);
+	if (neb_evdp_queue_attach(evdp_queue, s) != 0) {
+		fprintf(stderr, "failed to attach source\n");
+		neb_evdp_source_del(s);
+		goto exit_fail;
+	}
 
 	if (neb_evdp_queue_run(evdp_queue) != 0) {
 		fprintf(stderr, "error occure while running evdp queue\n");
@@ -85,13 +207,8 @@ int main(void)
 		ret = -1;
 
 exit_clean:
-	if (evdp_timer) {
-		if (send_tp)
-			neb_evdp_timer_del_point(evdp_timer, send_tp);
-		if (quit_tp)
-			neb_evdp_timer_del_point(evdp_timer, quit_tp);
+	if (evdp_timer)
 		neb_evdp_timer_destroy(evdp_timer);
-	}
 	if (evdp_queue)
 		neb_evdp_queue_destroy(evdp_queue);
 	return ret;
