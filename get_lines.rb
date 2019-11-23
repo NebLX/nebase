f585346d79da3f785036c5780907644e35fb12d1
Zhang Jingqiang <zh_jq@outlook.com>
2019-11-23 17:24:51 +0800

    allow to specify repo dir

--- get_lines.rb
+++ get_lines.rb
@@ -3,8 +3,8 @@
 require 'rugged'
 require 'linguist'
 
-path = File.dirname(__FILE__)
-rugged = Rugged::Repository.new(path)
+repopath = ARGV[0] || File.dirname(__FILE__)
+rugged = Rugged::Repository.new(repopath)
 repo = Linguist::Repository.new(rugged, rugged.head.target_id)
 
 file_breakdown = repo.breakdown_by_file
@@ -13,7 +13,8 @@ puts "------- --------"
 file_breakdown.each do |lang, files|
   lines = 0
   files.each do |file|
-    count = File.foreach(file).inject(0) {|c, line| c+1}
+    filepath = File.join(repopath, file)
+    count = File.foreach(filepath).inject(0) {|c, line| c+1}
     lines += count
   end
   puts "%7s %s" % ["#{lines}", lang]
