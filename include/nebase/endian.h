c3028e3dc9d3a87584afe87ef0d8f9183f0b9e0f
Zhang Jingqiang <zh_jq@outlook.com>
2019-12-20 23:54:54 +0800

    endian: simplify macro names

--- include/nebase/endian.h
+++ include/nebase/endian.h
@@ -13,17 +13,17 @@
  * Compile time swap
  */
 
-#define neb_constant_swab16(x) ((uint16_t)(                       \
+#define neb_const_swab16(x) ((uint16_t)(                       \
     (((uint16_t)(x) & (uint16_t)0x00ffU) << 8) |                  \
     (((uint16_t)(x) & (uint16_t)0xff00U) >> 8)))
 
-#define neb_constant_swab32(x) ((uint32_t)(                       \
+#define neb_const_swab32(x) ((uint32_t)(                       \
     (((uint32_t)(x) & (uint32_t)0x000000ffUL) << 24) |            \
     (((uint32_t)(x) & (uint32_t)0x0000ff00UL) <<  8) |            \
     (((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) |            \
     (((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24)))
 
-#define neb_constant_swab64(x) ((uint64_t)(                       \
+#define neb_const_swab64(x) ((uint64_t)(                       \
     (((uint64_t)(x) & (uint64_t)0x00000000000000ffULL) << 56) |   \
     (((uint64_t)(x) & (uint64_t)0x000000000000ff00ULL) << 40) |   \
     (((uint64_t)(x) & (uint64_t)0x0000000000ff0000ULL) << 24) |   \
@@ -34,35 +34,35 @@
     (((uint64_t)(x) & (uint64_t)0xff00000000000000ULL) >> 56)))
 
 #if BYTE_ORDER == LITTLE_ENDIAN
-# define neb_constant_htole16(x) (x)
-# define neb_constant_htobe16(x) neb_constant_swab16(x)
-# define neb_constant_le16toh(x) (x)
-# define neb_constant_be16toh(x) neb_constant_swab16(x)
+# define neb_const_htole16(x) (x)
+# define neb_const_htobe16(x) neb_const_swab16(x)
+# define neb_const_le16toh(x) (x)
+# define neb_const_be16toh(x) neb_const_swab16(x)
 
-# define neb_constant_htole32(x) (x)
-# define neb_constant_htobe32(x) neb_constant_swab32(x)
-# define neb_constant_le32toh(x) (x)
-# define neb_constant_be32toh(x) neb_constant_swab32(x)
+# define neb_const_htole32(x) (x)
+# define neb_const_htobe32(x) neb_const_swab32(x)
+# define neb_const_le32toh(x) (x)
+# define neb_const_be32toh(x) neb_const_swab32(x)
 
-# define neb_constant_htole64(x) (x)
-# define neb_constant_htobe64(x) neb_constant_swab64(x)
-# define neb_constant_le64toh(x) (x)
-# define neb_constant_be64toh(x) neb_constant_swab64(x)
+# define neb_const_htole64(x) (x)
+# define neb_const_htobe64(x) neb_const_swab64(x)
+# define neb_const_le64toh(x) (x)
+# define neb_const_be64toh(x) neb_const_swab64(x)
 #else
-# define neb_constant_htole16(x) neb_constant_swab16(x)
-# define neb_constant_htobe16(x) (x)
-# define neb_constant_le16toh(x) neb_constant_swab16(x)
-# define neb_constant_be16toh(x) (x)
+# define neb_const_htole16(x) neb_const_swab16(x)
+# define neb_const_htobe16(x) (x)
+# define neb_const_le16toh(x) neb_const_swab16(x)
+# define neb_const_be16toh(x) (x)
 
-# define neb_constant_htole32(x) neb_constant_swab32(x)
-# define neb_constant_htobe32(x) (x)
-# define neb_constant_le32toh(x) neb_constant_swab32(x)
-# define neb_constant_be32toh(x) (x)
+# define neb_const_htole32(x) neb_const_swab32(x)
+# define neb_const_htobe32(x) (x)
+# define neb_const_le32toh(x) neb_const_swab32(x)
+# define neb_const_be32toh(x) (x)
 
-# define neb_constant_htole64(x) neb_constant_swab64(x)
-# define neb_constant_htobe64(x) (x)
-# define neb_constant_le64toh(x) neb_constant_swab64(x)
-# define neb_constant_be64toh(x) (x)
+# define neb_const_htole64(x) neb_const_swab64(x)
+# define neb_const_htobe64(x) (x)
+# define neb_const_le64toh(x) neb_const_swab64(x)
+# define neb_const_be64toh(x) (x)
 #endif
 
 #endif
