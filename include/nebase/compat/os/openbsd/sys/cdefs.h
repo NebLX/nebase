a8eef7337a795baad893c65499053db2bafd4c58
Zhang Jingqiang <zh_jq@outlook.com>
2020-06-14 18:00:02 +0800

    update embeded gunlib hash

--- include/nebase/compat/os/openbsd/sys/cdefs.h
+++ include/nebase/compat/os/openbsd/sys/cdefs.h
@@ -13,4 +13,8 @@
 # define _GL_ATTRIBUTE_PURE __attribute__((__pure__))
 #endif
 
+#ifndef _GL_ATTRIBUTE_NODISCARD
+# define _GL_ATTRIBUTE_NODISCARD __attribute__((__warn_unused_result__))
+#endif
+
 #endif
