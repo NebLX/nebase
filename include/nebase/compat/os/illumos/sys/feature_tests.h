ade5d49f5068a9d8676cdfbdabeb65dbb65d2f02
Zhang Jingqiang <zh_jq@outlook.com>
2022-02-02 11:49:19 +0800

    be compitable with the new gnulib hash

--- include/nebase/compat/os/illumos/sys/feature_tests.h
+++ include/nebase/compat/os/illumos/sys/feature_tests.h
@@ -19,12 +19,24 @@
 # define _GL_ATTRIBUTE_PURE __attribute__((__pure__))
 #endif
 
+#ifndef _GL_ATTRIBUTE_MALLOC
+# define _GL_ATTRIBUTE_MALLOC __attribute__((__malloc__))
+#endif
+
+#ifndef _GL_ATTRIBUTE_DEALLOC
+# define _GL_ATTRIBUTE_DEALLOC(deallocator, ptr_index) __attribute__((__malloc__(deallocator, ptr_index)))
+#endif
+
 #ifndef _GL_ATTRIBUTE_NODISCARD
 # define _GL_ATTRIBUTE_NODISCARD __attribute__((__warn_unused_result__))
 #endif
 
 #ifndef _GL_ATTRIBUTE_DEPRECATED
 # define _GL_ATTRIBUTE_DEPRECATED __attribute__((__deprecated__))
+#endif
+
+#ifndef _GL_ATTRIBUTE_RETURNS_NONNULL
+# define _GL_ATTRIBUTE_RETURNS_NONNULL __attribute__((__returns_nonnull__))
 #endif
 
 #endif
